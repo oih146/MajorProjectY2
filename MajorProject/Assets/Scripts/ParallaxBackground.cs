c8668272f10572f19e8bdc3108bb9bd781ef861d
oih146 <t.sobb97@yahoo.com.au>
2017-08-10 15:19:37 +1000

    Started 'Parallaxing' Background

---
+++ MajorProject/Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+
+    public Transform m_parallaxTarget;
+    public float m_moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(PlayerMovement.m_amMoving)
+        {
+            Vector3 buff = gameObject.transform.position;
+            buff.x = Mathf.Lerp(buff.x, m_parallaxTarget.position.x, m_moveSpeed * Time.deltaTime);
+            gameObject.transform.position = buff;
+        }
+	}
+}
