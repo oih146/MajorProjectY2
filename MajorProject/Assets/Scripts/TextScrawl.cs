655582d8a9e000954149a615ab793fcbe306ac70
MSI\Tyrone <t.sobb97@yahoo.com.au>
2017-08-12 23:12:20 +1000

    Changes to bring project back up to prototype barring specific tweeks Added better scroll to inital text

--- MajorProject/Assets/Scripts/TextScrawl.cs
+++ MajorProject/Assets/Scripts/TextScrawl.cs
@@ -9,25 +9,50 @@ public class TextScrawl : MonoBehaviour {
     public UnityEngine.UI.Text textBox;
     public float m_yPosLimit;
     public float m_riseSpeed;
+    float m_timeSinceStart;
+    float m_initYPos;
 	// Use this for initialization
 	void Start () {
-
+        m_initYPos = textBox.transform.localPosition.y;
+        m_timeSinceStart = Time.time;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (textBox.gameObject.transform.localPosition.y < m_yPosLimit - 30)
+        //if (textBox.gameObject.transform.localPosition.y < m_yPosLimit - 100)
+        //{
+        //    Vector3 buff = textBox.transform.localPosition;
+        //    buff.y = Mathf.Lerp(buff.y, m_yPosLimit, m_riseSpeed);
+        //    textBox.transform.localPosition = buff;
+        //}
+        //else if(textBox.gameObject.transform.localPosition.y > m_yPosLimit - 100 || Input.GetButtonDown("Jump"))
+        //{
+        //    textBox.transform.parent.gameObject.SetActive(false);
+
+        //    fadeScreen.m_fadeIn = true;
+        //    fadeScreen.m_fading = true;
+        //    gameObject.SetActive(false);
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        if (textBox.gameObject.transform.localPosition.y < m_yPosLimit - 1f)
         {
+            float timeSinceLerp = Time.time - m_timeSinceStart;
+            float percentageComplete = timeSinceLerp / m_riseSpeed;
+
             Vector3 buff = textBox.transform.localPosition;
-            buff.y = Mathf.Lerp(buff.y, m_yPosLimit, m_riseSpeed);
+            buff.y = Mathf.Lerp(m_initYPos, m_yPosLimit, percentageComplete);
             textBox.transform.localPosition = buff;
         }
-        else
+        else if (textBox.gameObject.transform.localPosition.y > m_yPosLimit - 1f || Input.GetButtonDown("Jump"))
         {
             textBox.transform.parent.gameObject.SetActive(false);
-            gameObject.SetActive(false);
+
             fadeScreen.m_fadeIn = true;
             fadeScreen.m_fading = true;
+            gameObject.SetActive(false);
         }
     }
 }
