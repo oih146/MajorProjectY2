001df3d0886641a8b7c13477490bf197bd5ee9da
David K <toffeegoat@gmail.com>
2017-09-14 15:06:50 +1000

    Added ShaderStack, added particles.

---
+++ MajorProject/Assets/Scripts/ParticleSystems/ParticleSpimn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Update()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        transform.Rotate(Vector3.right * Time.deltaTime);
+
+    }
+}
