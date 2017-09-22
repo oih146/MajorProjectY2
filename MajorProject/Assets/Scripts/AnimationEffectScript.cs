0aa855564c06d7e0c9715c22e65a6631ff9d1332
oih146 <t.sobb97@yahoo.com.au>
2017-09-22 14:11:23 +1000

    Animation effects added

--- MajorProject/Assets/Scripts/AnimationEffectScript.cs
+++ MajorProject/Assets/Scripts/AnimationEffectScript.cs
@@ -19,15 +19,30 @@ public class AnimationEffectScript : MonoBehaviour {
 		
 	}
 
-    public void PlayEffect()
+    public AnimatorStateInfo GetAnimatorStateInfo()
     {
+        return m_animator.GetCurrentAnimatorStateInfo(0);
+    }
+
+    public ParticleSystem GetParticleSystem()
+    {
+        return m_partSys;
+    }
+
+    public IEnumerator PlayEffect()
+    {
+        gameObject.SetActive(true);
         m_animator.Play(m_anim.name);
         m_partSys.Play(true);
+        yield return new WaitForSeconds(1.0f);
+        yield return new WaitUntil(() => !m_animator.GetCurrentAnimatorStateInfo(0).IsName(m_anim.name));
+        StopEffect();
     }
 
     public void StopEffect()
     {
         m_animator.Stop();
         m_partSys.Stop();
+        gameObject.SetActive(false);
     }
 }
