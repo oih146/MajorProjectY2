121b539f1fb4fdc879508dce5e7b11aaf4c14873
David K <toffeegoat@gmail.com>
2017-09-22 15:41:58 +1000

    Menu work + busts

---
+++ MajorProject/Assets/Particle/ParticleScript/ShaderAdjPulse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderAdjPulse : MonoBehaviour { 
+
+
+
+
+    public Color colour = new Color(0.2F, 0.3F, 0.4F, 0.5F);
+
+    void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Material mat = renderer.material;
+
+        float floor = 0.3f;
+        float ceiling = 1.0f;
+        float emission = floor + Mathf.PingPong(Time.time, ceiling - floor);
+
+
+        Color finalColor = colour * Mathf.LinearToGammaSpace(emission);
+
+        mat.SetColor("_EmissionColor", finalColor);
+    }
+}
