23b6f2253e9226a347ea236da44beb38a5158e08
oih146 <t.sobb97@yahoo.com.au>
2017-10-26 15:25:14 +1100

    Added Camera lookup movement Changed and rearranged game time script

--- MajorProject/Assets/Scripts/GameTime.cs
+++ MajorProject/Assets/Scripts/GameTime.cs
@@ -5,27 +5,25 @@ using PixelCrushers.DialogueSystem;
 
 public class GameTime : MonoBehaviour {
 
-    static UnityEngine.UI.Text m_timeText;
+    public static GameTime Instance;
 
-    static int m_hours;
-    public static int Hours
-    {
-        get
-        {
-            return m_hours;
-        }
-    }
-    static int m_days;
-    public static int Days
-    {
-        get
-        {
-            return m_days;
-        }
-    }
+    public GameObject m_lightSource;
+    UnityEngine.UI.Text m_timeText;
+
+    float m_timeSinceStart;
+    public float m_lerpSpeed;
+    bool m_lerping;
+    float m_initalXRot;
+    float m_toXRot;
+
+    int m_hours;
+    public int Hours { get { return m_hours; } }
+    int m_days;
+    public int Days { get { return m_days; } }
 
     void Awake()
     {
+        Instance = this;
         m_timeText = GetComponent<UnityEngine.UI.Text>();
     }
 
@@ -36,7 +34,8 @@ public class GameTime : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (m_lerping)
+            LerpBody();
 	}
 
     void OnEnable()
@@ -50,8 +49,31 @@ public class GameTime : MonoBehaviour {
         ConversationEvents.OnConversationStart -= SetDialogueVariables;
         ConversationEvents.AfterPlayerResponse -= UpdateDialogueVariables;
     }
+    
+    void LerpBody()
+    {
+        float timeInLerp = Time.time - m_timeSinceStart;
+        float percentage = timeInLerp / m_lerpSpeed;
 
-    public static void AddHours(int amount)
+        Vector3 temp =  new Vector3 (m_lightSource.transform.eulerAngles.x, 0 , 0);
+        temp.x = Mathf.Lerp(m_initalXRot, m_toXRot, percentage);
+        //temp.x = (temp.x > 360 ? temp.x / 360 :  temp.x + 0);
+        m_lightSource.transform.eulerAngles = temp;
+        if(percentage >= 1f)
+        {
+            m_lerping = false;
+        }
+    }
+
+    void StartLerp(float newXRot)
+    {
+        m_timeSinceStart = Time.time;
+        m_initalXRot = m_lightSource.transform.eulerAngles.x;
+        m_toXRot =  m_initalXRot + newXRot;
+        m_lerping = true;
+    }
+
+    public void AddHours(int amount)
     {
         m_hours += amount;
         int toAdd = 0;
@@ -64,13 +86,16 @@ public class GameTime : MonoBehaviour {
         if (toAdd > 0)
             AddDays(toAdd);
         else
+        {
+            StartLerp((amount / 24.0f) * 360);
             UpdateTimeText();
+        }
     }
 
-    public static void AddDays(int amount)
+    public void AddDays(int amount)
     {
         m_days += amount;
-        UpdateTimeText();
+        StartLerp((amount / 24) * 360);
     }
 
     //For Sequence on Dialogue System
@@ -103,7 +128,7 @@ public class GameTime : MonoBehaviour {
     //    UpdateTimeText();
     //}
 
-    public static int[] GetTime()
+    public int[] GetTime()
     {
         int[] timeArray = new int[4];
         timeArray[2] = m_hours;
@@ -111,12 +136,15 @@ public class GameTime : MonoBehaviour {
         return timeArray;
     }
 
-    static void UpdateTimeText()
+    void UpdateTimeText()
     {
         m_timeText.text = "Days: " + Days + "\nHours: " + Hours;
     }
 
-
+    void UpdateLight(float amountToRot)
+    {
+        m_lightSource.transform.eulerAngles = new Vector3(m_lightSource.transform.eulerAngles.x + amountToRot, 0, 0);
+    }
 
     public void UpdateDialogueVariables()
     {
