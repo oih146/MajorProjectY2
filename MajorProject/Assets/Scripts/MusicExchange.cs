f0533a6f0dd27f008e4ff01c559888817fcc026d
oih146 <t.sobb97@yahoo.com.au>
2017-11-10 11:55:04 +1100

    More bug fixes

--- MajorProject/Assets/Scripts/MusicExchange.cs
+++ MajorProject/Assets/Scripts/MusicExchange.cs
@@ -11,8 +11,6 @@ public class MusicExchange : MonoBehaviour {
     }
 
     public MusicSource m_musicSource;
-
-
     public AudioClip m_newClip;
 
 	// Use this for initialization
@@ -22,14 +20,15 @@ public class MusicExchange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player")
         {
-            ExchangeClip();
+            if(enabled)
+                ExchangeClip();
         }
     }
 
@@ -38,17 +37,21 @@ public class MusicExchange : MonoBehaviour {
         if (m_musicSource == MusicSource.Combat)
         {
             float time = MusicSwitcher.Instance.m_battleAudio.time;
+            float volume = MusicSwitcher.Instance.m_battleAudio.volume;
             MusicSwitcher.Instance.m_battleAudio.Stop();
             MusicSwitcher.Instance.m_battleAudio.clip = m_newClip;
             MusicSwitcher.Instance.m_battleAudio.time = time;
+            MusicSwitcher.Instance.m_battleAudio.volume = volume;
             MusicSwitcher.Instance.m_battleAudio.Play();
         }
         else if (m_musicSource == MusicSource.Regular)
         {
             float time = MusicSwitcher.Instance.m_normalAudio.time;
+            float volume = MusicSwitcher.Instance.m_normalAudio.volume;
             MusicSwitcher.Instance.m_normalAudio.Stop();
             MusicSwitcher.Instance.m_normalAudio.clip = m_newClip;
             MusicSwitcher.Instance.m_normalAudio.time = time;
+            MusicSwitcher.Instance.m_normalAudio.volume = volume;
             MusicSwitcher.Instance.m_normalAudio.Play();
         }
     }
